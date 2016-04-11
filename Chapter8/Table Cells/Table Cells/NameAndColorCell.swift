b8fcfe548f3211a15fe7627c2ae092f77bb38f76
daichao <wuxirendaichao@aliyun.com>
2016-04-11 23:40:06 +0800

    从nib文件加载UITableViewCell

--- Chapter8/Table Cells/Table Cells/NameAndColorCell.swift
+++ Chapter8/Table Cells/Table Cells/NameAndColorCell.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class NameAndColorCell: UITableViewCell {
 
-    var name:String=""
-    var color:String=""
+    var name:String=""{
+        didSet{
+            if(name != oldValue){
+                nameLabel.text=name
+            }
+        }
+    }
+    var color:String=""{
+        didSet{
+            if(color != oldValue){
+                colorLabel.text=color
+            }
+        }
+    }
     var nameLabel:UILabel!
     var colorLabel:UILabel!
+    
     
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -26,7 +39,26 @@ class NameAndColorCell: UITableViewCell {
         nameMarker.font=UIFont.boldSystemFontOfSize(12)
         contentView.addSubview(nameMarker)
         
+        let colorLabelRect=CGRectMake(0, 26, 70, 15)
+        let colorMarker=UILabel(frame: colorLabelRect)
+        colorMarker.textAlignment=NSTextAlignment.Right
+        colorMarker.text="Color:"
+        colorMarker.font=UIFont.boldSystemFontOfSize(12)
+        contentView.addSubview(colorMarker)
         
+        
+        let nameValueRect=CGRectMake(80, 5, 200, 15)
+        nameLabel=UILabel(frame:nameValueRect)
+        contentView.addSubview(nameLabel)
+        
+        let colorValueRect=CGRectMake(80, 25, 200, 15)
+        colorLabel=UILabel(frame: colorValueRect)
+        contentView.addSubview(colorLabel)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func awakeFromNib() {
